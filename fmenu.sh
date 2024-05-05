3981ba075192b7cd0c2aa97240a1d14e02036d6d
AntyElo <nougatoelo@yandex.com>
2024-05-06 01:34:32 +0300

    It now shoud work

--- fmenu.sh
+++ fmenu.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-#source "$CURRENT_DIR/shared.sh"
-
-echo "tmux menu -T fmenu -x P -y S 'Send fmenu hotkey' C-t 'send C-t' `for i in $(ls $CURRENT_DIR/apps/) ; do cat $CURRENT_DIR/apps/$i done`" | bash
+P=""
+for i in $(ls $CURRENT_DIR/apps/) ; do
+    P="$P `cat $CURRENT_DIR/apps/$i`"
+done
+P="tmux menu -T fmenu -x P -y S 'Send fmenu hotkey' C-t 'send C-t'$P"
+bash -c "$P"
